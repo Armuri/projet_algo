b2752e97f365d6cb74fc35bc2b5e2ce9051b7684
RonanSaves01 <ronan.saves@epsi.fr>
2023-04-11 10:26:24 +0200

    10:25 11/04

---
+++ système check-in,out.py
@@ -0,0 +1,15 @@
+import livres
+import utilisateurs
+
+livre-empruntés = 0
+
+
+
+r = input('Quel utilisateur vous voulez voir?')
+if r == nom and r == prénom:
+    print('type: '+ type)
+    print('Nombres de livres empruntés:'+ len(livres[]))
+    
+    
+
+    
