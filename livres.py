b6201c9c2a2dcdcb4af5c444bb7c791bf8e3d07a
Armuri <nguyenarthur4@gmail.com>
2023-04-11 09:59:08 +0200

    09:58 11/04/2023

--- livres.py
+++ livres.py
@@ -1,3 +1,23 @@
-class livres : 
+import json
+
+class Livre : 
     def __init__(self,auteur, titre, editeur, annee, genre, disponible):
-        self.auteur 
+        self.auteur = auteur
+        self.titre = titre
+        self.editeur = editeur
+        self.annee = annee
+        self.genre = genre
+        self.disponible = disponible
+        
+    def __str__(self):
+        return "Ce livre est un/une : " + self.genre + " du nom de : " + self.titre + " écrit par : " + self.auteur + ',' " édité par : " + self.editeur + ' publié en : ' + str(int(self.annee)) +  " et sa disponibilité est : " + self.disponible 
+        
+f = open("livres.json", "r")
+content = json.load(f)
+
+tab = []
+for d in content:
+    tab.append(Livre(d['auteur'],d['titre'],d['editeur'],d['annee'],d['genre'],d['disponible']))
+
+for livre in tab:
+    print(livre) 
