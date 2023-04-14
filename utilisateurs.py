13fe86ca996f20fc799f7d04688eeaada6a9417a
Armuri <nguyenarthur4@gmail.com>
2023-04-14 09:06:56 +0200

    09:06

--- utilisateurs.py
+++ utilisateurs.py
@@ -1,5 +1,22 @@
 import json
 
-# Charge les données JSON
-with open('utilisateurs.json') as f:
-    utilisateurs = json.load(f)
+class Utilisateur : 
+    def __init__(self,nom, prenom, adresse_email, type, livres):
+        self.nom = nom
+        self.prenom = prenom
+        self.adresse_email = adresse_email
+        self.type = type
+        self.livres = livres
+        
+    def __str__(self):
+        return self.nom  + '\n' + self.prenom  + '\n' + self.adresse_email + '\n' +  self.type + '\n' + str(self.livres)
+       
+f = open("utilisateurs.json", "r")
+content = json.load(f)
+tab = []
+for d in content:
+    tab.append(Utilisateur(d['nom'],d['prenom'],d['adresse_email'],d['type'],d['livres']))
+
+for user in tab:
+    print(user)
+
